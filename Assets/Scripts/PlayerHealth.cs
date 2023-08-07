feb2b36b425651e1eb194d55c8fb86d7e959972d
STINKYZIPPYKENZ <parkerjames@mmc.school.nz>
2023-08-07 12:38:26 +1200

    more

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private float health;
+    private float lerpTimer;
+    public float maxHealth = 100f;
+    public float chipSpeed = 2f;
+    public Image frontHealthBar;
+    public Image backHealthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = Mathf.Clamp(health, 0, maxHealth);
+        UpdateHealthUI();
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(Random.Range(5, 10));
+        }
+    }
+    public void UpdateHealthUI()
+    {
+        Debug.Log(health);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        lerpTimer = 0f;
+    }
+}
